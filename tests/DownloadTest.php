9fe9824181e3ba4991612377cbc011b79426f2c2
walker <showphp@foxmail.com>
2018-03-11 23:35:11 +0800

    编写下载文件接口的单元测试

--- tests/DownloadTest.php
+++ tests/DownloadTest.php
@@ -2,16 +2,38 @@
 use PHPUnit\Framework\TestCase;
 require './src/Download.php';
 
-class DownloadTest extends  TestCase {
+class DownloadTest extends  TestCase
+{
+    protected $isSkipTest = true;
+    protected $downloadObj;
+
+    protected function setUp()
+    {
+        $this->downloadObj = new Download();
+        $this->downloadObj->remoteFileUrl = 'http://bpic.588ku.com/element_origin_min_pic/18/03/02/c5757c2830b83fe6489f0b0c5a5d7c48.jpg';
+
+    }
 
     /**
-     * @expectedException Exception
+     * @return mixed
      */
     public function testPrepareDownload()
     {
-        $download = new Download();
-        $download->remoteFileUrl = 'http://bpic.588ku.com/element_origin_min_pic/18/03/02/c5757c2830b83fe6489f0b0c5a5d7c48.jpg';
-        $result = $download->prepareDownload();
-        print_r($result);
+        $result = $this->downloadObj->prepareDownload();
+        $this->assertNotEmpty($result);
+        return $result;
     }
+
+
+    /**
+     * @depends testPrepareDownload
+     * @param array $file_arr
+     */
+    public function testStartDownload(array $file_arr)
+    {
+        $this->assertNotEmpty($file_arr);
+        $this->downloadObj->startDownload($file_arr['tmp_file']);
+        $this->assertFileExists($file_arr['tmp_file']);
+    }
+
 }
